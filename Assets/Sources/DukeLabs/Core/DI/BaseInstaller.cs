c00306537ed400b5ab4f57294c49a7bf897a4a70
AlexanderDykov <alexander.dykov@gmail.com>
2017-11-07 11:48:01 +0200

    [MOD] replace UpdateManager singleton with Zenject DI

---
+++ Assets/Sources/DukeLabs/Core/DI/BaseInstaller.cs
@@ -0,0 +1,13 @@
+﻿using DukeLabs.Core.UI;
+using Zenject;
+
+namespace DukeLabs.Core.DI
+{
+    public class BaseInstaller : MonoInstaller<BaseInstaller>
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<UpdateManager>().FromNewComponentOnNewGameObject().AsSingle();
+        }
+    }
+}
