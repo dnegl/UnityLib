827cad75b6ddbe55d2998934d0e476fe2bcfc586
d.neglyada <d.neglyada@room8studio.com>
2018-10-31 15:09:42 +0200

    Start point for the test project

---
+++ Assets/Sources/DukeLabs/Core/StartPoint.cs
@@ -0,0 +1,39 @@
+﻿    using Assets.Sources.Game.ECS.Features;
+using Entitas.VisualDebugging.Unity;
+using UnityEngine;
+
+namespace DukeLabs.Core
+{
+	//Start point for the project 
+    public class StartPoint : MonoBehaviour
+    {
+        [SerializeField] private Transform _parent;
+        [SerializeField] private Transform _scoreParent;
+        private Entitas.Systems _systems;
+
+        void Awake()
+        {
+            var contexts = Contexts.sharedInstance;
+            _systems = new GameFeature(contexts, _parent, _scoreParent);
+        }
+
+        void Start()
+        {
+            _systems.Initialize();
+        }
+
+        void Update()
+        {
+            _systems.Execute();
+            _systems.Cleanup();
+        }
+
+        void OnDestroy()
+        {
+            _systems.TearDown();
+            _systems.DeactivateReactiveSystems();
+            Contexts.sharedInstance.Reset();
+            Contexts.sharedInstance = null;
+        }
+    }
+}
